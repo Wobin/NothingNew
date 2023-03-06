fb8c9731d56c7b11229841b4df44b41b97a623bb
deluxghost <deluxghost@users.noreply.github.com>
2023-03-06 17:05:20 +0800

    add zh-cn localization

--- scripts/mods/Nothing New/Nothing New_localization.lua
+++ scripts/mods/Nothing New/Nothing New_localization.lua
@@ -1,14 +1,18 @@
 return {
   mod_title = {
-    en = "Nothing New"
-    },
+    en = "Nothing New",
+		["zh-cn"] = "没有新物品",
+  },
 	mod_description = {
 		en = "Cleans up the 'new item' notification dot",
+		["zh-cn"] = "清除所有“新物品”黄点",
 	},
   turn_on_cleaner = {
-    en = "Remove new item indicators"
+    en = "Remove new item indicators",
+		["zh-cn"] = "清除新物品黄点",
   },
   nothing_new_tooltip = {
-    en = "This will auto-toggle off"
-    }
+    en = "This will auto-toggle off",
+		["zh-cn"] = "此选项会自动关闭",
+  }
 }
